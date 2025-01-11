593e259f20ddde31a1b52181838f99a954aec02f
Emmanuel Ogidan <emmanuelogidan@bepeerless.co>
2025-01-11 01:51:47 +0100

    11th jan push

--- AppEntry/Program.cs
+++ AppEntry/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Kata8;
 
-Console.WriteLine(sumStrings.sumString("123", "456"));
+
+
+string input = "543210";
+
+int length = input.Length % 2 ==0 ? input.Length/2 : (input.Length/2)+1;
+for(int x =0; x < length; x++)
+{
+
+}
